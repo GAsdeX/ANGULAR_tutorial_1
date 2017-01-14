b36e85de9f52b8863f9c8c78a292eb05ffbb67f0
GAsdeX <grinya2102@gmail.com>
2017-01-14 14:41:14 +0200

    есть подгрузка 4-х героев и переходы

--- app/main.js
+++ app/main.js
@@ -12,19 +12,67 @@ search_app.controller('myCtrl', function($scope) {
     }
 });
 
-search_app.controller('heroes_arr', function($scope, $log) {
+search_app.controller('main', function($scope, $log) {
+
+    $scope.apperaMain = true;
+    $scope.apperaHeroes = false;
+    $scope.apperaDeshboard = false;
+
     $scope.heroes = [
       {
-        name : "Grosha"
+        name : "Grosha",
+        id:"11"
       },{
-        name : "ABBA"
+        name : "ABBA",
+        id:"12"
       },{
-        name : "GAsdeX"
+        name : "GAsdeX",
+        id:"13"
       },{
-        name : "Hooray"
+        name : "Hooray",
+        id:"14"
+      },{
+        name : "Grosha",
+        id:"15"
+      },{
+        name : "ABBA",
+        id:"16"
+      },{
+        name : "GAsdeX",
+        id:"17"
+      },{
+        name : "Hooray",
+        id:"18"
+      },{
+        name : "Hooray",
+        id:"19"
+      },{
+        name : "Hooray",
+        id:"20"
       }
     ]
+    console.log($scope.heroes[0]['id']);
+    $scope.printHeroes = function(){
+      var heroesArr =[]
+      for (var i = 0; $scope.heroes[i]; i++){
+        if (i<4){
+          heroesArr.push($scope.heroes[i])
+          console.log(heroesArr[i]);
+        }
+      }
+      console.log(heroesArr);
+      return heroesArr;
+    }
+    $scope.heroes_4 = $scope.printHeroes()
+
+    $scope.showHeroes = function () {
+      $scope.apperaMain = false;
+      $scope.apperaHeroes = true;
+      console.log('hhh');
+    }
+    $scope.showMain = function () {
+      $scope.apperaMain = true;
+      $scope.apperaHeroes = false;
+      console.log('hhh');
+    }
 });
-
-
-console.log('sdfsvcscdfvcdfvfxd');
